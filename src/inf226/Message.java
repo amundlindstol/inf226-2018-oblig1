d412749811cc7a2a3e0d562db31ff699eeadab6b
Håkon Robbestad Gylterud <hakon.gylterud@uib.no>
2018-09-21 09:06:34 +0200

    Message constructor now takes String sender

--- src/inf226/Message.java
+++ src/inf226/Message.java
@@ -3,8 +3,8 @@ package inf226;
 public class Message {
 	public final String sender, recipient, message;
 	
-	Message(final User user, final String recipient, final String message) throws Invalid {
-		this.sender = user.getName();
+	Message(final String sender, final String recipient, final String message) throws Invalid {
+		this.sender = sender;
 		this.recipient = recipient;
 		if (!valid(message))
 			throw new Invalid(message);
